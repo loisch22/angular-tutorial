306f675126a56a219a1bf0a29abd38a4b5bf772e
Lois Choi <loisch22@gmail.com>
2017-11-22 14:21:33 -0800

    start angularjs tutorial

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+//create a module (container) called myModule that has no dependencies
+var myApp = angular.module("myModule", []);
+
+// //create controller (JS function)
+// var myController = function($scope) {
+//   $scope.message = "AngularJS Tutorial";
+// }
+//
+// //register controller with module
+// //name of the controller and name of function
+// myApp.controller("myController", myController);
+
+//can refactor like this:
+myApp.controller("myController", function($scope) {
+  //complex object
+  var employee = {
+    firstName: "David",
+    lastName: "Hastings",
+    gender: "Male"
+  }
+  //change $scope to employee object as model being passed
+  $scope.employee = employee;
+
+  // $scope.message = "AngularJS Tutorial";
+});
