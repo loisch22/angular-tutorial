fd3f0ee226336e7c9c5e74646acae71cb69e9b39
Lois Choi <loisch22@gmail.com>
2017-11-22 15:58:07 -0800

    use method chaining to write module and register controller in one line

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,38 @@
 //create a module (container) called myModule that has no dependencies
-var myApp = angular.module("myModule", []);
-
-// //create controller (JS function)
-// var myController = function($scope) {
-//   $scope.message = "AngularJS Tutorial";
-// }
+// var myApp = angular.module("myModule", []);
 //
-// //register controller with module
-// //name of the controller and name of function
-// myApp.controller("myController", myController);
+// // //create controller (JS function)
+// // var myController = function($scope) {
+// //   $scope.message = "AngularJS Tutorial";
+// // }
+// //
+// // //register controller with module
+// // //name of the controller and name of function
+// // myApp.controller("myController", myController);
+//
+// //can refactor like this:
+// myApp.controller("myController", function($scope) {
+//   //complex object
+//   var employee = {
+//     firstName: "David",
+//     lastName: "Hastings",
+//     gender: "Male"
+//   }
+//   //change $scope to employee object as model being passed
+//   $scope.employee = employee;
+//
+//   // $scope.message = "AngularJS Tutorial";
+// });
 
-//can refactor like this:
-myApp.controller("myController", function($scope) {
-  //complex object
-  var employee = {
-    firstName: "David",
-    lastName: "Hastings",
-    gender: "Male"
-  }
-  //change $scope to employee object as model being passed
-  $scope.employee = employee;
-
-  // $scope.message = "AngularJS Tutorial";
-});
+//method chaining
+var myApp = angular
+      .module("myModule", [])
+      .controller("myController", function($scope) {
+        var employee = {
+          firstName: "David",
+          lastName: "Hastings",
+          gender: "Male"
+        };
+        
+        $scope.employee = employee;
+      });
