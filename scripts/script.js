9a6c85e9a0e3cdce73c3369df72652f05e7e41f1
Lois Choi <loisch22@gmail.com>
2017-11-22 19:45:48 -0800

    create custom filter in script. 1st param filter name. create function pass gender aka 1,2,3; then use switch method to set values

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,29 @@
 //displaying images dynamically
 var myApp = angular
       .module("myModule", [])
+      .filter("gender", function() {
+        return function(gender) {
+          switch(gender) {
+            case 1:
+              return "Male";
+            case 2:
+              return "Female";
+            case 3:
+              return "Not disclosed";
+          }
+        }
+      })
       .controller("myController", function($scope) {
         var employees = [
-          { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788, city: "Seattle" },
-          { name: "Susan", dateOfBirth: new Date("January 23, 1970"), gender: "Female", salary: 68000, city: "San Francisco" },
-          { name: "Sara", dateOfBirth: new Date("March 10, 1984"), gender: "Female", salary: 57000, city: "Seattle" },
-          { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 53000, city: "San Francisco" },
-          { name: "Todd", dateOfBirth: new Date("December 05, 1983"), gender: "Male", salary: 60000, city: "San Francisco" }
+          { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: 1, salary: 55000.788, city: "Seattle" },
+          { name: "Susan", dateOfBirth: new Date("January 23, 1970"), gender: 2, salary: 68000, city: "San Francisco" },
+          { name: "Sara", dateOfBirth: new Date("March 10, 1984"), gender: 2, salary: 57000, city: "Seattle" },
+          { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: 1, salary: 53000, city: "San Francisco" },
+          { name: "Todd", dateOfBirth: new Date("December 05, 1983"), gender: 3, salary: 60000, city: "San Francisco" }
         ];
         $scope.employees = employees;
 
-        //item = all the properties listed in td
-        $scope.search = function (item) {
-          if($scope.searchText == undefined) {
-            //returns all data
-            return true;
-          } else {
-            //indexOf looks for matching value returns -1 if no match is found
-            if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 || item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
-              return true;
-            }
-          }
-          //does not show if it doesn't match criteria
-          return false;
-        }
+
 
 
       });
