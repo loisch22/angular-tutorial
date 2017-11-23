3af39f94ed71c593af711dd33532e318cd36f610
Lois Choi <loisch22@gmail.com>
2017-11-22 19:16:01 -0800

    add input for name and city then boolean checkbox for exact match

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ var myApp = angular
       .module("myModule", [])
       .controller("myController", function($scope) {
         var employees = [
-          { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788 },
-          { name: "Susan", dateOfBirth: new Date("January 23, 1970"), gender: "Female", salary: 68000 },
-          { name: "Sara", dateOfBirth: new Date("March 10, 1984"), gender: "Female", salary: 57000 },
-          { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 53000 },
-          { name: "Todd", dateOfBirth: new Date("December 05, 1983"), gender: "Male", salary: 60000 }
+          { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788, city: "Seattle" },
+          { name: "Susan", dateOfBirth: new Date("January 23, 1970"), gender: "Female", salary: 68000, city: "San Francisco" },
+          { name: "Sara", dateOfBirth: new Date("March 10, 1984"), gender: "Female", salary: 57000, city: "Seattle" },
+          { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 53000, city: "San Francisco" },
+          { name: "Todd", dateOfBirth: new Date("December 05, 1983"), gender: "Male", salary: 60000, city: "San Francisco" }
         ];
         $scope.employees = employees;
-    
+
 
 
 
