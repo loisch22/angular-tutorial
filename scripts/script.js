4ab8d04ca85a0a89ce4623cf9f959f0677ba1ec8
Lois Choi <loisch22@gmail.com>
2017-11-22 17:40:13 -0800

    add filters - trickiest: number:2 determines how many decimal places to show / currency:'£':1 changes symbol to pounds and displays one decimal

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,14 @@
 var myApp = angular
       .module("myModule", [])
       .controller("myController", function($scope) {
-        var technologies = [
-          { name: "C#", likes: 0, dislikes: 0 },
-          { name: "ASP.NET", likes: 0, dislikes: 0 },
-          { name: "SQL Server", likes: 0, dislikes: 0 },
-          { name: "AngularJS", likes: 0, dislikes: 0 }
+        var employees = [
+          { name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: 55000.788 },
+          { name: "Susan", dateOfBirth: new Date("January 23, 1970"), gender: "Female", salary: 68000 },
+          { name: "Sara", dateOfBirth: new Date("March 10, 1984"), gender: "Female", salary: 57000 },
+          { name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: 53000 },
+          { name: "Todd", dateOfBirth: new Date("December 05, 1983"), gender: "Male", salary: 60000 }
         ];
-        $scope.technologies = technologies;
-        //increment likes/dislikes
-        $scope.incrementLikes = function (technology) {
-          technology.likes++;
-        };
-        $scope.incrementDislikes = function (technology) {
-          technology.dislikes++;
-        };
+        $scope.employees = employees;
       });
 
 //method chaining - displaying properties
